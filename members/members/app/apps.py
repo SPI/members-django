e4d59a4fd7d6be6c12a595e86331ffb0a11ce83a
Célestin Matte <gitlab@cmatte.me>
2022-02-19 18:16:49 +0100

    apps.py: remove code that's not necessary for now

--- members/members/app/apps.py
+++ members/members/app/apps.py
@@ -5,7 +5,7 @@ class AppAppConfig(AppConfig):
     name = 'members.app'
     default_auto_field = 'django.db.models.BigAutoField'
 
-    def ready(self):
-        from members.auth import auth_user_data_received
+#    def ready(self):
+#        from members.auth import auth_user_data_received
 #        from .migration import handle_user_data
-        auth_user_data_received.connect(handle_user_data)
+#        auth_user_data_received.connect(handle_user_data)
