c94569afdf6abd07a635f71fff74dfca5b51914a
Célestin Matte <gitlab@cmatte.me>
2022-02-19 19:39:16 +0100

    Model: make members inherit from Django's auth User

--- members/members/app/models.py
+++ members/members/app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Members(models.Model):
-    memid = models.AutoField(null=False, primary_key=True)
+    memid = models.OneToOneField(User, null=False, blank=False, primary_key=True, on_delete=models.RESTRICT, db_column='memid')
     name = models.CharField(max_length=50, null=False)
     email = models.CharField(max_length=50, null=False)
     phone = models.CharField(max_length=20)
