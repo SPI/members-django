91c25fc7f50263c6439b80547d903fcd195ca206
Célestin Matte <gitlab@cmatte.me>
2022-02-19 18:06:27 +0100

    pycodestyle

--- members/members/app/apps.py
+++ members/members/app/apps.py
@@ -6,7 +6,6 @@ class AppAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
-#        from .migration import handle_user_data
         from members.auth import auth_user_data_received
-
+#        from .migration import handle_user_data
         auth_user_data_received.connect(handle_user_data)
