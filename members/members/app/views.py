923885403ed9be5bc11afff0385450c0a68cd9e4
Célestin Matte <gitlab@cmatte.me>
2022-02-20 12:37:40 +0100

    Start importing and adapting views: base.html

--- members/members/app/views.py
+++ members/members/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    template = loader.get_template('base.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
