f2e229ad36565528d96d2c90a23c3a889fcc43c2
Célestin Matte <celestin.matte@gmail.com>
2022-03-29 09:53:46 +0200

    Advancing on status view

--- members/membersapp/app/applications.py
+++ members/membersapp/app/applications.py
@@ -1,6 +1,7 @@
 from django.db import connection
 
 from .models import Members
+from .utils import *
 
 
 def get_applications(self, manager=None):
@@ -8,19 +9,15 @@ def get_applications(self, manager=None):
         applications = []
         cur = connection.cursor()
         if manager:
-            if self.data['dbtype'] == 'sqlite3':
-                cur.execute('SELECT a.*, m.* from applications a, members m ' +
-                            'WHERE m.memid = a.member AND a.manager = ? ' +
-                            'ORDER BY a.appdate', (manager.memid, ))
-            elif self.data['dbtype'] == 'postgres':
                 cur.execute('SELECT a.*, m.* from applications a, members m ' +
                             'WHERE m.memid = a.member AND a.manager = %s ' +
                             'ORDER BY a.appdate', (manager.memid, ))
         else:
             cur.execute('SELECT a.*, m.* from applications a, members m ' +
                         'WHERE m.memid = a.member ORDER BY a.appdate')
-        for row in cur.fetchall():
+        for row in dictfetchall(cur):
             if not manager or manager.memid != row['manager']:
+                print(row['manager'])
                 manager, _ = Members.object.get(pk=row['manager'])
             applications.append(self.application_from_db(row))
         return applications
