c1582a0405220f563240ad5dde9388ed51a1242f
Célestin Matte <gitlab@cmatte.me>
2022-04-16 20:45:54 +0200

    Add migration.py for user connection

---
+++ members/membersapp/app/migration.py
@@ -0,0 +1,14 @@
+from django.db import transaction
+from django.conf import settings
+
+from .models import Members
+
+
+@transaction.atomic
+def handle_user_data(sender, user, userdata, **kwargs):
+    # This shouldn't happen if we're not using pg auth, but just to be sure
+    if not settings.USE_PG_COMMUNITY_AUTH:
+        return
+
+    # Make sure this user has a subscriber record
+    subscriber, _ = Members.object.get_or_create(name=user)
