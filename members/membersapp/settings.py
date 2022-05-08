0583959c1f032057f344ff646ae8bca9315930c6
Célestin Matte <gitlab@cmatte.me>
2022-05-08 17:58:22 +0200

    Change default static folder to /media

--- members/membersapp/settings.py
+++ members/membersapp/settings.py
@@ -126,7 +126,7 @@ DATETIME_INPUT_FORMATS = ['%Y-%m-%d']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
