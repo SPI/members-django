b2f29bbcf00cbf05cea059cc2ec13a8bf1764d72
Célestin Matte <gitlab@cmatte.me>
2022-05-08 15:14:05 +0200

    settings.py: add STATIC_ROOT for deployment

--- members/membersapp/settings.py
+++ members/membersapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,6 +146,7 @@ PGAUTH_CHANGEPASSWD = "https://pgweb.spi-inc.org/account/changepwd"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, "./media/")
 
 try:
     from .local_settings import *
