62817b4dd07155b18c0d1c7ea0ac1f9dcff5af33
Célestin Matte <gitlab@cmatte.me>
2022-05-13 20:46:56 +0200

    import script: don't import users that didn't validate email

--- import_from_members_additional_fixes.sql
+++ import_from_members_additional_fixes.sql
@@ -1,3 +1,3 @@
-insert into auth_user (id, email, date_joined, password, is_superuser, is_staff, is_active, username, first_name, last_name) select memid, email, coalesce(firstdate, NOW()), 'imported', false, false, true, memid, '', '' from members;
+insert into auth_user (id, email, date_joined, password, is_superuser, is_staff, is_active, username, first_name, last_name) select memid, email, coalesce(firstdate, NOW()), 'imported', false, false, true, memid, '', '' from members where ismember = true;
 
 SELECT setval(pg_get_serial_sequence('"auth_user"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "auth_user";
