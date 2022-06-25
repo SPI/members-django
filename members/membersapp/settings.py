d1a465ca2da2acbb9a5567646524fe5c6e2e5904
Célestin Matte <gitlab@cmatte.me>
2022-06-25 19:41:45 +0200

    Set Debug to False by default

--- members/membersapp/settings.py
+++ members/membersapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l_+ya#^z)@&o^%k*z$)-z!=dh$es_9i!w!+=z8-wg4nub@e!#g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
