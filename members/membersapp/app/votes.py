52e0361ee576ae9e88c377a9631911f1fac40eb9
Célestin Matte <gitlab@cmatte.me>
2022-10-07 16:49:31 +0200

    Import voting system from previous member app (depends on obsolete openstv)

--- members/membersapp/app/votes.py
+++ members/membersapp/app/votes.py
@@ -1,4 +1,5 @@
 from django.db import connection
+from openstv.plugins import getMethodPlugins
 
 from .models import Members
 
@@ -7,3 +8,107 @@ VOTE_SYSTEMS = [(0, "Condorcet (ignore unspecified)"),
                 (1, "Condorcet (unspecified ranked lowest)"),
                 (2, "Scottish STV"),
                 ]
+
+
+class CondorcetVS(object):
+    """Implementation of the Condorcet voting system"""
+    def __init__(self, vote, membervotes, ignoremissing=True):
+        self.vote = vote
+        self.membervotes = membervotes
+        self.ignoremissing = ignoremissing
+        # Initialise our empty beat matrix
+        self.beatmatrix = {}
+        for row in self.vote.options:
+            self.beatmatrix[row.optionid] = {}
+            for col in self.vote.options:
+                self.beatmatrix[row.optionid][col.optionid] = 0
+        self.tie = False
+        self.winners = [None] * len(self.vote.options)
+        self.wincount = {}
+
+    def description(self):
+        if self.ignoremissing:
+            return "Condorcet (ignore unspecified)"
+        else:
+            return "Condorcet (unspecified ranked lowest)"
+
+    def run(self):
+        """Run the vote"""
+        options = [option.optionid for option in self.vote.options]
+
+        # Fill the beat matrix. bm[x][y] is the number of times x was
+        # preferred over y.
+        for membervote in self.membervotes:
+            votecounted = {}
+            for curpref, pref in enumerate(membervote.votes):
+                votecounted[pref.optionid] = True
+                for lesspref in membervote.votes[curpref + 1:]:
+                    self.beatmatrix[pref.optionid][lesspref.optionid] += 1
+
+                # If we're not ignoring missing options then treat them
+                # as lower preference than anyone who was listed.
+                if not self.ignoremissing:
+                    for missing in options:
+                        # Check it's actually missing
+                        if missing not in votecounted:
+                            for counted in votecounted:
+                                self.beatmatrix[counted][missing] += 1
+
+        for row in options:
+            wins = 0
+            self.wincount[row] = {}
+            for col in options:
+                if row != col:
+                    self.wincount[row][col] = (self.beatmatrix[row][col] -
+                                               self.beatmatrix[col][row])
+                    if self.wincount[row][col] > 0:
+                        wins += 1
+
+            self.wincount[row]['wins'] = wins
+
+            if self.winners[wins]:
+                self.tie = True
+                self.winners[wins] += " AND "
+                self.winners[wins] += self.vote.option_by_ref(row).description
+            else:
+                self.winners[wins] = self.vote.option_by_ref(row).description
+
+    def results(self):
+        """Return an array of the vote winners"""
+        return reversed(self.winners)
+
+
+class OpenSTVVS(object):
+    """Implementation of various STV voting systems using OpenSTV"""
+    def __init__(self, vote, membervotes, system="ScottishSTV"):
+        self.vote = vote
+        self.membervotes = membervotes
+        self.tie = False
+        methods = getMethodPlugins("byName", exclude0=False)
+        if system not in methods:
+            raise Exception("Unknown OpenSTV method " + system)
+        self.system = methods[system]
+        self.election = None
+
+    def description(self):
+        """Return a text string describing the voting system"""
+        return self.system.longMethodName + " (OpenSTV)"
+
+    def run(self):
+        """Run the vote using the OpenSTV backend"""
+        loader = SPIBallotLoader(self.vote, self.membervotes)
+        dirty = openstv.ballots.Ballots()
+        dirty.loader = loader
+        loader.loadballots(dirty)
+        clean = dirty.getCleanBallots()
+        self.election = self.system(clean)
+        self.election.runElection()
+
+    def results(self):
+        """Return an array of the vote winners"""
+        winners = list(self.election.winners)
+        winners.sort()
+        if len(winners) == 0:
+            return None
+        else:
+            return [self.election.b.names[c] for c in winners]
