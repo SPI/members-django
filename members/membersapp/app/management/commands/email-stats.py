afc0203d8c94dcc7b69e2bf7ceda993af9eb8d52
Célestin Matte <gitlab@cmatte.me>
2022-10-25 18:52:08 +0200

    Add email-stats as a django command

---
+++ members/membersapp/app/management/commands/email-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import datetime
+from smtplib import SMTPException
+
+from django.core.management.base import BaseCommand, CommandError
+from django.template import loader
+from django.conf import settings
+from django.core.mail import send_mail
+
+from membersapp.app.stats import get_stats
+
+
+class Command(BaseCommand):
+    help = 'Send email containing stats on members and applications'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--email', dest='email',
+                            help="Email address to send message",
+                            action='store', default='board@spi-inc.org, membership@spi-inc.org')
+        parser.add_argument('--dry-run', dest='dryrun',
+                            help="Just show what would happen, don't take any action",
+                            action='store_const', const=True, default=False)
+
+    def handle(self, *args, **options):
+        template = loader.get_template('stats.txt')      
+        context = {
+            'stats': get_stats(),
+            'date': '{:%F %T}'.format(datetime.datetime.today())
+        }
+        msg = template.render(context)
+        if not options['dryrun']:
+            try:
+                send_mail('SPI membership statistics', msg, 'SPI Membership Committee <membership@spi-inc.org>', options['email'], fail_silently=False)
+            except (SMTPException, ConnectionRefusedError):
+                raise CommandError('Unable to send contributing member confirmation email.')
+        else:
+            self.stdout.write(msg)
