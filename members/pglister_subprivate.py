a6a537a6e8a15cb8b46d4b4f1d5b0beb4305b74e
Célestin Matte <gitlab@cmatte.me>
2022-10-31 10:36:49 +0100

    pglister_subprivate.py: add parameters to configure url and host header

--- members/pglister_subprivate.py
+++ members/pglister_subprivate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import urllib.request
+import ssl
 
 from django.core.management.base import BaseCommand, CommandError
 from django.template import loader
@@ -14,12 +15,22 @@ class Command(BaseCommand):
     help = 'Prompt the members application for contributing members list and subscribe them to the spi-private mailing list'
 
     def add_arguments(self, parser):
+        parser.add_argument('hostname',
+                            help="Hostname of target URL, to be used in header of request.")
         parser.add_argument('--dry-run', dest='dryrun',
                             help="Just show what would happen, don't take any action",
                             action='store_const', const=True, default=False)
+        parser.add_argument('--url', dest='url',
+                            help="URL of the members application",
+                            action='store', default='https://localhost')
 
     def handle(self, *args, **options):
-        url = 'https://beta.membersdjango.spi-inc.org/privatesubs'
-        response = urllib.request.urlopen(url)
+        url = '%s/privatesubs' % options['url']
+        r = urllib.request.Request(url, headers={'Host': options['hostname']})
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+        response = urllib.request.urlopen(r, context=ctx)
         data = response.read()
         addresses = data.decode('utf-8').split('\n')
+        print(addresses)
