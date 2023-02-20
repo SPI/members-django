352d8431ba79feec8b2d175d4c78740bc6ec4d1a
Célestin Matte <gitlab@cmatte.me>
2023-02-20 18:47:36 +0100

    pgweb integration: cleaning: remove unused functions in decorators.py

--- members/membersapp/account/util/decorators.py
+++ members/membersapp/account/util/decorators.py
@@ -4,26 +4,6 @@ from collections import defaultdict
 from django.contrib.auth.decorators import login_required as django_login_required
 
 
-def nocache(fn):
-    def _nocache(request, *_args, **_kwargs):
-        resp = fn(request, *_args, **_kwargs)
-        resp['Cache-Control'] = 's-maxage=0'
-        return resp
-    return _nocache
-
-
-def cache(days=0, hours=0, minutes=0, seconds=0):
-    "Set the server to cache object a specified time. td must be a timedelta object"
-    def _cache(fn):
-        def __cache(request, *_args, **_kwargs):
-            resp = fn(request, *_args, **_kwargs)
-            td = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
-            resp['Cache-Control'] = 's-maxage=%s' % (td.days * 3600 * 24 + td.seconds)
-            return resp
-        return __cache
-    return _cache
-
-
 def queryparams(*args):
     """
     Allow specified query parameters when calling function.
@@ -33,14 +13,6 @@ def queryparams(*args):
         fn.queryparams = args
         return fn
     return _queryparams
-
-
-def allow_frames(fn):
-    def _allow_frames(request, *_args, **_kwargs):
-        resp = fn(request, *_args, **_kwargs)
-        resp.x_allow_frames = True
-        return resp
-    return _allow_frames
 
 
 def content_sources(what, source):
