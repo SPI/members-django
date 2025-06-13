cef626cac2781afb8dd89e0c6e3be77912a129b4
Célestin Matte <dev@cmatte.me>
2025-06-13 17:29:55 +0200

    Vote edit: display error when form is incorrect

--- members/membersapp/app/views.py
+++ members/membersapp/app/views.py
@@ -390,6 +390,9 @@ def voteeditedit(request, ref):
                 form.instance.owner = user
                 form.save()
                 return HttpResponseRedirect(reverse('voteedit', args=(ref,)))
+            else:
+                messages.error(request, "Error while filling the form:")
+                messages.error(request, form.errors)
         elif request.POST['vote-btn'] == "Delete":
             VoteOption.objects.filter(election_ref=vote).delete()
             vote.delete()
