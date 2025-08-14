33009276c085690ad894f4e8982915bfa99f8e91
Célestin Matte <dev@cmatte.me>
2025-08-14 16:43:43 +0200

    Bugfix: count contrib members in the stats page using the contrib field instead of applications
    An elected NCM director becomes contrib automatically, which
    means that all contrib members won't necessarily have an
    application.

--- members/membersapp/app/stats.py
+++ members/membersapp/app/stats.py
@@ -1,5 +1,7 @@
 from django.db import connection
 
+from .models import Members
+
 
 def type_to_sql(listtype):
     """Build the WHERE clause for looking up members by type"""
@@ -14,9 +16,9 @@ def type_to_sql(listtype):
     elif listtype == 'ca':
         where = ('AND m.ismember = true AND a.approve IS NULL AND ' +
                  'contribapp = true')
-    elif listtype == 'cm':
-        where = ('AND m.ismember = true AND m.iscontrib = true '
-                 ' AND contribapp = true')
+#    elif listtype == 'cm':
+#        where = ('AND m.ismember = true AND m.iscontrib = true '
+#                 ' AND contribapp = true')
     elif listtype == 'mgr':
         where = ('AND m.ismember = true AND m.ismanager = true '
                  ' AND contribapp = true')
@@ -30,7 +32,7 @@ def get_stats():
     """Retrieve membership statistics"""
     stats = {}
 
-    for listtype in ['nca', 'ncm', 'ca', 'cm', 'mgr']:
+    for listtype in ['nca', 'ncm', 'ca', 'mgr']:
         where = type_to_sql(listtype)
         with connection.cursor() as cursor:
             cursor.execute('SELECT COUNT(memid) AS count FROM applications a, ' +
@@ -38,5 +40,11 @@ def get_stats():
             row = cursor.fetchone()
         if row:
             stats[listtype] = row[0]
+
+    # An elected NCM director becomes contrib automatically, which
+    # means that all contrib members won't necessarily have an
+    # application. Hence, count contrib members directly instead of
+    # using applications
+    stats['cm'] = Members.objects.filter(iscontrib=True).count()
 
     return stats
