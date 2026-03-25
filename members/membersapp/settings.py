a93ccef17a08f711119369e93705fc947695ba09
Célestin Matte <dev@cmatte.me>
2026-03-25 12:23:27 +0100

    Move /media to /static and fix settings

--- members/membersapp/settings.py
+++ members/membersapp/settings.py
@@ -135,11 +135,13 @@ DATETIME_INPUT_FORMATS = ['%Y-%m-%d']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/media/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_prod")
 
 # account
 ACCOUNTS_NOREPLY_FROM = "pgweb@localhost"                # Address to send account messages from
@@ -174,7 +176,6 @@ REIMBURSEMENTS_URL = 'https://reimbursements.spi-inc.org'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, "./media/")
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = '25'
